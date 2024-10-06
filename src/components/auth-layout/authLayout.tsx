f0e9b55744247134bd826784b8dbcdc6be3f2a70
Jmjoss <josekandia42@gmail.com>
2024-10-06 15:36:51 -0300

    modifica el layout

--- src/components/auth-layout/authLayout.tsx
+++ src/components/auth-layout/authLayout.tsx
@@ -11,7 +11,7 @@ export default function AuthLayout({ children }: AuthLayoutProps) {
                 <Logo />
             </div>
 
-            <div className="w-full md:w-1/2 flex items-center justify-center bg-white">
+            <div className="w-full md:w-1/2 flex items-center justify-center bg-[#f5f5f5]">
                 {children}
             </div>
         </div>

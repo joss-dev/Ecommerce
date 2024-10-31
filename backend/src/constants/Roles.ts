11aadc4991ee10816f012327f1fdac0bad075257
Jmjoss <josekandia42@gmail.com>
2024-10-31 17:21:01 -0300

    modifica el roles

--- backend/src/constants/Roles.ts
+++ backend/src/constants/Roles.ts
@@ -1,5 +1,4 @@
 export enum Roles {
     ADMIN = "admin",
-    DOCTOR = "doctor",
-    PATIENT = "customer",
+    CUSTOMER = "customer",
 }
--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,22 +1,22 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get("token")?.value; // Lee el token desde las cookies
+  const token = request.cookies.get("token")?.value; 
   const url = request.nextUrl.clone();
 
   const isAuthenticated = !!token;
 
-  // Rutas accesibles solo sin autenticación (login, register)
+  
   const unauthenticatedRoutes = ["/login", "/signup"];
   if (isAuthenticated && unauthenticatedRoutes.includes(url.pathname)) {
-    url.pathname = "/"; // Redirige al dashboard si el usuario ya está autenticado
+    url.pathname = "/"; 
     return NextResponse.redirect(url);
   }
 
-  // Rutas protegidas que requieren autenticación
+
   const protectedRoutes = ["/dashboard", "/profile"];
   if (!isAuthenticated && protectedRoutes.includes(url.pathname)) {
-    url.pathname = "/login"; // Redirige al login si el usuario no está autenticado
+    url.pathname = "/login"; 
     return NextResponse.redirect(url);
   }
 

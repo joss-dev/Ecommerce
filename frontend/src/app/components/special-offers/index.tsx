db4c5a399a1bd7e5085acbaf7e3cf0593bf79993
Jmjoss <josekandia42@gmail.com>
2024-11-02 10:19:52 -0300

    agrega try catch

--- frontend/src/app/components/special-offers/index.tsx
+++ frontend/src/app/components/special-offers/index.tsx
@@ -5,26 +5,35 @@ import { fetchProducts } from "@/app/lib/data";
 import { Product } from "@/app/types/types";
 
 const ProductList = async () => {
-  const products = await fetchProducts(); 
-  return (
-    <div className="flex flex-wrap justify-center gap-4 m-2">
-      {products.map((product: Product) => (
-        <ProductCard
-          id={product.id}
-          key={product.id}
-          imageUrl={product.imageUrl || ""}
-          title={product.name}
-          description={product.description}
-          price={product.price}
-          oldPrice={product.price * 1.2} 
-          discount="20%" 
-          rating={product.stars}
-          reviewCount={100} 
-        />
-      ))}
-    </div>
-  );
-};
+  try {
+    const products = await fetchProducts();
+    return (
+      <div className="flex flex-wrap justify-center gap-4 m-2">
+        {products.map((product: Product) => (
+          <ProductCard
+            id={product.id}
+            key={product.id}
+            imageUrl={product.imageUrl || ""}
+            title={product.name}
+            description={product.description}
+            price={product.price}
+            oldPrice={product.price * 1.2}
+            discount="20%"
+            rating={product.stars}
+            reviewCount={100}
+          />
+        ))}
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    return (
+      <div className="text-center text-red-500 p-4">
+        Hubo un problema al cargar los productos. Por favor, intenta nuevamente más tarde.
+      </div>
+    );
+  };
+}
 
 export default function SpecialOffers() {
   return (
@@ -35,7 +44,7 @@ export default function SpecialOffers() {
         ))}
       </div>
     }>
-      <ProductList /> 
+      <ProductList />
     </Suspense>
   );
 }
--- frontend/src/app/products/[id]/page.tsx
+++ frontend/src/app/products/[id]/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import React, { useEffect, useState } from "react";
 import { getProductByID } from "@/app/lib/data";
 import { Product } from "@/app/types/types";
